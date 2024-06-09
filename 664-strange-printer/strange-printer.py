f59ee5b7d49014cf15ee241e08ad908d03c34223
Aneesh495 <179398431+Aneesh495@users.noreply.github.com>
2024-06-09 06:47:09 +0000

    Time: 155 ms (66.39%) | Memory: 19.4 MB (93.33%) - LeetSync

---
+++ 664-strange-printer/strange-printer.py
@@ -0,0 +1,26 @@
+
+class Solution:
+    def strangePrinter(self, s: str) -> int:
+        if not s:
+            return 0
+
+        # Remove consecutive duplicates to reduce state size
+        t = []
+        for ch in s:
+            if not t or t[-1] != ch:
+                t.append(ch)
+        s = ''.join(t)
+        n = len(s)
+
+        dp = [[0] * n for _ in range(n)]
+        for i in range(n):
+            dp[i][i] = 1
+
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                dp[i][j] = dp[i][j - 1] + 1
+                for k in range(i, j):
+                    if s[k] == s[j]:
+                        dp[i][j] = min(dp[i][j], dp[i][k] + (dp[k + 1][j - 1] if k + 1 <= j - 1 else 0))
+        return dp[0][n - 1]
