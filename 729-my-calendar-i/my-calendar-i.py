8327085c115d724b81ac65b5108adda51ff05de2
Aneesh495 <179398431+Aneesh495@users.noreply.github.com>
2024-09-06 16:59:05 +0000

    Time: 35 ms (84.00%) | Memory: 20.2 MB (57.79%) - LeetSync

---
+++ 729-my-calendar-i/my-calendar-i.py
@@ -0,0 +1,18 @@
+
+import bisect
+
+class MyCalendar:
+
+    def __init__(self):
+        self.starts = []
+        self.ends = []
+
+    def book(self, start: int, end: int) -> bool:
+        i = bisect.bisect_left(self.starts, start)
+        if i > 0 and self.ends[i - 1] > start:
+            return False
+        if i < len(self.starts) and self.starts[i] < end:
+            return False
+        self.starts.insert(i, start)
+        self.ends.insert(i, end)
+        return True
