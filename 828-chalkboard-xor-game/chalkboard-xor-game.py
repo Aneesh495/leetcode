01e1bf9e5db2a2bac8c81f6200da3b16a5433ec1
Aneesh495 <179398431+Aneesh495@users.noreply.github.com>
2024-12-30 10:56:17 +0000

    Time: 0 ms (100.00%) | Memory: 19.4 MB (47.77%) - LeetSync

---
+++ 828-chalkboard-xor-game/chalkboard-xor-game.py
@@ -0,0 +1,9 @@
+
+from typing import List
+
+class Solution:
+    def xorGame(self, nums: List[int]) -> bool:
+        x = 0
+        for v in nums:
+            x ^= v
+        return x == 0 or len(nums) % 2 == 0
