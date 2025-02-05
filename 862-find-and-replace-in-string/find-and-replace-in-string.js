9bb8817aa5537f36e2c70002ea55083006328899
Aneesh495 <179398431+Aneesh495@users.noreply.github.com>
2025-02-05 15:30:40 +0000

    Time: 0 ms (100.00%) | Memory: 54.1 MB (88.46%) - LeetSync

---
+++ 862-find-and-replace-in-string/find-and-replace-in-string.js
@@ -0,0 +1,1 @@
+const findReplaceString=(n,t,e,c)=>{const o=[...n];return t.forEach(((t,i)=>{const l=e[i],r=c[i];if(n.slice(t,t+l.length)===l){o[t]=r;for(let n=1;n<l.length;n++)o[t+n]=""}})),o.join("")};
