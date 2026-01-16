ddbe1337160c393326618890aad4f781a8bb271c
Aneesh495 <179398431+Aneesh495@users.noreply.github.com>
2026-01-16 07:25:45 +0000

    Time: 0 ms (100.00%) | Memory: 9.1 MB (61.72%) - LeetSync

---
+++ 1156-occurrences-after-bigram/occurrences-after-bigram.cpp
@@ -0,0 +1,14 @@
+
+class Solution {
+public:
+    vector<string> findOcurrences(string text, string first, string second) {
+        vector<string> words, res;
+        stringstream ss(text);
+        string word;
+        while (ss >> word) words.push_back(word);
+        for (int i = 0; i + 2 < words.size(); ++i)
+            if (words[i] == first && words[i + 1] == second)
+                res.push_back(words[i + 2]);
+        return res;
+    }
+};
