03055a0c5a01414ba520c80d550940b50d8d5016
Aneesh Krishna <aneeshkrishnaparthasarathy@gmail.com>
2026-04-03 22:53:51 -0400

    Time: 0 ms (100.00%) | Memory: 9.6 MB (7.61%) - LeetSync

---
+++ 1203-print-in-order/print-in-order.cpp
@@ -0,0 +1,28 @@
+
+class Foo {
+private:
+    mutex m1, m2;
+public:
+    Foo() {
+        m1.lock();
+        m2.lock();
+    }
+
+    void first(function<void()> printFirst) {
+        printFirst();
+        m1.unlock();
+    }
+
+    void second(function<void()> printSecond) {
+        m1.lock();
+        printSecond();
+        m1.unlock();
+        m2.unlock();
+    }
+
+    void third(function<void()> printThird) {
+        m2.lock();
+        printThird();
+        m2.unlock();
+    }
+};
