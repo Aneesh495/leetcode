574b175984be3cef189dc5d3e8a1bd582a42ba8d
Aneesh Krishna <aneeshkrishnaparthasarathy@gmail.com>
2026-04-03 22:59:46 -0400

    Time: 0 ms (100.00%) | Memory: 26.9 MB (75.73%) - LeetSync

---
+++ 1219-longest-well-performing-interval/longest-well-performing-interval.cpp
@@ -0,0 +1,17 @@
+
+class Solution {
+public:
+    int longestWPI(vector<int>& hours) {
+        int n = hours.size(), res = 0, score = 0;
+        unordered_map<int, int> seen;
+        for (int i = 0; i < n; ++i) {
+            score += hours[i] > 8 ? 1 : -1;
+            if (score > 0) res = i + 1;
+            else {
+                if (!seen.count(score)) seen[score] = i;
+                if (seen.count(score - 1)) res = max(res, i - seen[score - 1]);
+            }
+        }
+        return res;
+    }
+};
