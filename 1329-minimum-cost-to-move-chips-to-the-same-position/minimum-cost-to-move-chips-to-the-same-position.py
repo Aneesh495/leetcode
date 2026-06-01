bc3b772fef0f3f9684749649d91d781c0f77ce6c
Aneesh495 <179398431+Aneesh495@users.noreply.github.com>
2026-06-01 06:50:41 +0000

    Time: 0 ms (100.00%) | Memory: 19.2 MB (49.16%) - LeetSync

---
+++ 1329-minimum-cost-to-move-chips-to-the-same-position/minimum-cost-to-move-chips-to-the-same-position.py
@@ -0,0 +1,7 @@
+from typing import List
+
+class Solution:
+    def minCostToMoveChips(self, position: List[int]) -> int:
+        odd = sum(p % 2 for p in position)
+        even = len(position) - odd
+        return min(odd, even)
