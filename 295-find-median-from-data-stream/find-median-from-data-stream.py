91bc5170de181e9da0a0ce1c70e0f1da6d468649
Aneesh495 <179398431+Aneesh495@users.noreply.github.com>
2026-07-29 10:52:48 +0000

    Time: 132 ms (93.86%) | Memory: 41.7 MB (75.42%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -1,21 +1,30 @@
-
 import heapq
 
 class MedianFinder:
+
     def __init__(self):
-        self.small = []  # max heap as negatives
-        self.large = []  # min heap
+        self.lower = []
+        self.upper = []
 
     def addNum(self, num: int) -> None:
-        heapq.heappush(self.small, -num)
-        if self.large and -self.small[0] > self.large[0]:
-            heapq.heappush(self.large, -heapq.heappop(self.small))
-        if len(self.small) > len(self.large) + 1:
-            heapq.heappush(self.large, -heapq.heappop(self.small))
-        if len(self.large) > len(self.small):
-            heapq.heappush(self.small, -heapq.heappop(self.large))
+        if not self.lower or num <= -self.lower[0]:
+            heapq.heappush(self.lower, -num)
+        else:
+            heapq.heappush(self.upper, num)
+        if len(self.lower) > len(self.upper) + 1:
+            largest_lower = -heapq.heappop(self.lower)
+            heapq.heappush(self.upper, largest_lower)
+        elif len(self.upper) > len(self.lower):
+            smallest_upper = heapq.heappop(self.upper)
+            heapq.heappush(self.lower, -smallest_upper)
 
     def findMedian(self) -> float:
-        if len(self.small) > len(self.large):
-            return float(-self.small[0])
-        return (-self.small[0] + self.large[0]) / 2.0
+        if len(self.lower) > len(self.upper):
+            return float(-self.lower[0])
+        return (-self.lower[0] + self.upper[0]) / 2.0
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
